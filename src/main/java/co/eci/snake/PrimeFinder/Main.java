ae746a6eedcd87b7ee2332b56fdc844721c45d98
Oscar Sanchez <oscar.sporras@mail.escuelaing.edu.co>
2026-01-30 09:11:06 -0500

    Archivos primeFinder

---
+++ src/main/java/co/eci/snake/PrimeFinder/Main.java
@@ -0,0 +1,12 @@
+package co.eci.snake.PrimeFinder;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Control control = Control.newControl();
+        
+        control.start();
+
+    }
+	
+}
